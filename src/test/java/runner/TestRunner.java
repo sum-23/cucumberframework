df6b1119b313c6c11e0f6a9d1be2c09c7563482a
sumit <krsumit944@gmail.com>
2025-07-12 12:50:20 +0530

    change runner class file name

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,6 +8,6 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/features", glue = { "stepdefinitions", "hooks" }, plugin = {
 		"html:target/cucumber/cucumberReport.html", "pretty" }, publish=true)
-public class MyRunner {
+public class TestRunner {
 
 }
